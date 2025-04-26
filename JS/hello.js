de6b2cf63ae178b75fd2a064c13fd845815ef0b7
munsifkhandev <munsifkhandev@gmail.com>
2025-04-26 12:41:59 +0500

    Introduction to Javascript

---
+++ JS/hello.js
@@ -0,0 +1,17 @@
+function printData() {
+  console.log("Yehh dekho print hogya");
+}
+printData();
+
+function bringChai(itne) {
+  console.log(`Bhaiyaa mujhyy ${itne} cup chai peeni haiii.`);
+}
+bringChai(3);
+
+function addTwo(num1, num2) {
+  sum = num1 + num2;
+  console.log(`the sum of two Numbers is equal to ${sum}`);
+}
+addTwo(4, 5);
+
+
