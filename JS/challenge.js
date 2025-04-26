5c7ce9220d6ab78ee24ba9296eae869ef3c7f02f
munsifkhandev <munsifkhandev@gmail.com>
2025-04-26 12:42:20 +0500

    Practice Set of Javascript

---
+++ JS/challenge.js
@@ -0,0 +1,13 @@
+// Challenge 1 :
+
+function totalCupcakes(trayOne, trayTwo) {
+  return trayOne + trayTwo;
+}
+console.log("Total number of Cupckaes Per Tray :", totalCupcakes(12, 20));
+
+// Challenge 2 :
+
+function totalChocolates(barPerBox, numberOfBoxes) {
+  return barPerBox * numberOfBoxes;
+}
+console.log("Total number of Chocolates Per box :", totalChocolates(9, 4));
