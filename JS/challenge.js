815a67c4902506e5c94f32490dd93df02604c419
munsifkhandev <munsifkhandev@gmail.com>
2025-04-26 23:41:23 +0500

    Did More Challenges

--- JS/challenge.js
+++ JS/challenge.js
@@ -11,3 +11,26 @@ function totalChocolates(barPerBox, numberOfBoxes) {
   return barPerBox * numberOfBoxes;
 }
 console.log("Total number of Chocolates Per box :", totalChocolates(9, 4));
+
+// Challenge 3 :
+
+function myName(name) {
+  console.log(`My name is ${name}`);
+}
+myName("Munsif Khan");
+
+// Challenge 4 :
+
+function totalStrength(Boys, Girls) {
+  return Boys - Girls;
+}
+console.log(totalStrength(38, 12));
+
+function myClass(larky, larkiyan) {
+  let totalStrength = 50;
+
+  console.log(`Total Strength is ${totalStrength}`);
+  console.log("Larkiyon ki strength :", totalStrength - larky);
+}
+
+myClass(38);
