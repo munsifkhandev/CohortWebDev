864a35c60f029cbe2287112f7e63205161650c5f
munsifkhandev <munsifkhandev@gmail.com>
2025-04-27 17:51:44 +0500

    Understanding Basic Datatypes

---
+++ JS/BootcampProfile.js
@@ -0,0 +1,29 @@
+// String
+let myName = "Munsif Khan";
+//Number
+let myAge = 22;
+// boolean
+let isPaid = true;
+//null
+let favouriteBook = null;
+// undefined
+let hometown;
+
+console.log(
+  `My name is ${myName} and I am ${myAge} years old , My internship is Paid and The Answer is ${isPaid} my favorite book is ${favouriteBook} and my hometown is ${hometown}`
+);
+
+//arrays
+
+let myArray = ["HTML", "JS", "NODEJS"];
+console.log(myArray);
+
+// objects
+
+let studentProfile = {
+  name: "Munsif Khan",
+  age: 21,
+  uni: "IIUI",
+  skills: ["Coding", "Cricket", "WWE"],
+};
+console.log(studentProfile);
